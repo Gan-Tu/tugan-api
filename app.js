bdf44b3a33324363ab31d4f9ac88eddca4bb62c1
Gan Tu <github@tugan.dev>
2021-02-11 15:10:47 -0800

    add body parser

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var bodyParser = require("body-parser");
+var logger = require("morgan");
 
 var indexRouter = require('./routes/index');
 
@@ -15,6 +16,8 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
