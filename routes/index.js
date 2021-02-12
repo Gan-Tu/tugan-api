043a75516e2bad073149cc514e540fd977509ff0
Gan Tu <github@tugan.dev>
2021-02-11 17:56:05 -0800

    add basic ping pong endpoint

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Gan's Personal API" });
 });
 
+/* GET ping pong. */
+router.get("/ping", function (req, res, next) {
+  res.status(200).end("pong");
+});
+
 module.exports = router;
